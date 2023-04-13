9864532030ecb259cf1fb9b0e1c9f51a25276152
GokuPlay609 <120821054+GokuPlay609@users.noreply.github.com>
2023-04-13 21:32:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 
   
     var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
-  }; 
+  apiKey: "AIzaSyB_BTmkZv18SDSNGarRUTtRLIGI4hg72gA",
+  authDomain: "testhag-1d7e7.firebaseapp.com",
+  databaseURL: "https://testhag-1d7e7-default-rtdb.firebaseio.com",
+  projectId: "testhag-1d7e7",
+  storageBucket: "testhag-1d7e7.appspot.com",
+  messagingSenderId: "546113034871",
+  appId: "1:546113034871:web:0fa07376ff39efd6404472"
+};
  
 ////Do Not Edit Below This
 
